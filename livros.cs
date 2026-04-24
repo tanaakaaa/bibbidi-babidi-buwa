c421666b462e1fa637e997e7e816b3ecd3306672
tanaakaaa <gustavoktanaka@gmail.com>
2026-04-24 07:41:48 -0300

    Create livros.cs

---
+++ livros.cs
@@ -0,0 +1,7 @@
+public class Livro
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; } = string.Empty;
+    public string Autor { get; set; } = string.Empty;
+    public DateTime DataPublicacao { get; set; }
+}
